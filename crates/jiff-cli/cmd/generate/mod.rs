b0b641dc5ba0f6b2b8ddb9da457917a1ae988884
Andrew Gallant <jamslam@gmail.com>
2025-02-24 16:11:18 -0500

    jiff-cli: add `jiff-cli generate shared` command
    This is responsible for copying `jiff::shared` into the proc
    macro. We do some minor transformations, but otherwise the
    code remains the same.

--- crates/jiff-cli/cmd/generate/mod.rs
+++ crates/jiff-cli/cmd/generate/mod.rs
@@ -1,6 +1,9 @@
+use anyhow::Context;
+
 use crate::args;
 
 mod crc32;
+mod shared;
 mod tzdb;
 mod unit_designator_match;
 mod windows_zones;
@@ -16,6 +19,7 @@ USAGE:
 COMMANDS:
     crc32                  Generate CRC32 data tables.
     jiff-tzdb              Generate Rust source code from TZif data for jiff-tzdb
+    shared                 Copy shared code from Jiff to jiff-tz-static proc macro
     unit-designator-match  Generate Rust `match` expression for parsing unit labels
     windows-zones          Generate mapping Windows TZ names to IANA names.
     zoneinfo               Generate TZif data for jiff-tzdb
@@ -25,9 +29,50 @@ pub fn run(p: &mut lexopt::Parser) -> anyhow::Result<()> {
     match &*args::next_as_command(USAGE, p)? {
         "crc32" => crc32::run(p),
         "jiff-tzdb" => tzdb::run(p),
+        "shared" => shared::run(p),
         "unit-designator-match" => unit_designator_match::run(p),
         "windows-zones" => windows_zones::run(p),
         "zoneinfo" => zoneinfo::run(p),
         unk => anyhow::bail!("unrecognized command '{}'", unk),
     }
+}
+
+/// Run rustfmt on the given crate.
+///
+/// I'm not sure why, but running `rustfmt` on files directly sometimes
+/// produces different output than running `cargo fmt`. Maybe `rustfmt`
+/// isn't respecting the configuration somehow?
+fn cargo_fmt(krate: &str) -> anyhow::Result<()> {
+    use std::process::Command;
+
+    let out = Command::new("cargo")
+        .arg("fmt")
+        .arg("-p")
+        .arg(krate)
+        .output()
+        .context("`cargo fmt` command failed")?;
+    anyhow::ensure!(
+        out.status.success(),
+        "cargo fmt -p {krate}: {}",
+        bstr::BString::from(out.stderr),
+    );
+    Ok(())
+}
+
+/// Run rustfmt on the given file path.
+fn rustfmt<P: AsRef<std::path::Path>>(path: P) -> anyhow::Result<()> {
+    use std::process::Command;
+
+    let path = path.as_ref();
+    let out = Command::new("rustfmt")
+        .arg(path)
+        .output()
+        .context("rustfmt command failed")?;
+    anyhow::ensure!(
+        out.status.success(),
+        "rustfmt {}: {}",
+        path.display(),
+        bstr::BString::from(out.stderr),
+    );
+    Ok(())
 }
