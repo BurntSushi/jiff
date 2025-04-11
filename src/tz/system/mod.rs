08903c55ec12a5cc83b52c0711cbf443d6d680b7
Joey de Waal <99046430+joeydewaal@users.noreply.github.com>
2025-04-11 21:03:46 +0200

    tz: treat an empty `TZ` environment variable as `TZ=UTC`
    Specifically, an environment variable that is *set* to
    the empty string.
    
    This seems like odd behavior to me, but it's apparently
    what both GNU and BSD tooling do. And since Jiff's
    support for `TZ` is really about conforming to existing
    convention, we just do what everyone else does.
    
    PR #319

--- src/tz/system/mod.rs
+++ src/tz/system/mod.rs
@@ -175,7 +175,10 @@ fn get_env_tz(db: &TimeZoneDatabase) -> Result<Option<TimeZone>, Error> {
 
     let Some(tzenv) = std::env::var_os("TZ") else { return Ok(None) };
     if tzenv.is_empty() {
-        return Ok(None);
+        // It is commonly agreed (across GNU and BSD tooling at least),
+        // but not standard, that setting an empty `TZ=` is indistinguishable
+        // from `TZ=UTC`.
+        return Ok(Some(TimeZone::UTC));
     }
     let tz_name_or_path = match PosixTzEnv::parse_os_str(&tzenv) {
         Err(_err) => {
