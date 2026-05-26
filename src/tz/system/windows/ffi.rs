341313fa023a8c4296e50dc7fc54de94b6f76a53
Andrew Gallant <jamslam@gmail.com>
2026-05-26 08:07:03 -0400

    msrv: fix Jiff build on Windows for Rust 1.71
    I think older Rust versions were more paranoid about leaking types with
    a more restricted scope. So just switch to `pub`.
    
    We test this in CI, but only the build. Since dev dependencies have much
    higher MSRVs.
    
    Fixes #566

--- src/tz/system/windows/ffi.rs
+++ src/tz/system/windows/ffi.rs
@@ -1,7 +1,12 @@
-pub(super) const TIME_ZONE_ID_INVALID: u32 = 0xffffffff;
+pub const TIME_ZONE_ID_INVALID: u32 = 0xffffffff;
 
+// MSRV(??): Must be marked as public to avoid errors in older Rust compilers.
+// Rust 1.71 needs this. But Rust 1.95 does not. I'm not sure where the cross
+// over point is, but it'd be nice to tighten this up a bit. In any case, this
+// isn't actually re-exported anywhere in Jiff's public API, so it's fine.
+// ---AG
 #[repr(C)]
-pub(super) struct DYNAMIC_TIME_ZONE_INFORMATION {
+pub struct DYNAMIC_TIME_ZONE_INFORMATION {
     bias: i32,
     standard_name: [u16; 32],
     standard_date: SYSTEMTIME,
@@ -9,8 +14,8 @@ pub(super) struct DYNAMIC_TIME_ZONE_INFORMATION {
     daylight_name: [u16; 32],
     daylight_date: SYSTEMTIME,
     daylight_bias: i32,
-    /// This is the only field we actually need.
-    pub(super) timezone_key_name: [u16; 128],
+    // This is the only field we actually need.
+    pub timezone_key_name: [u16; 128],
     dynamic_daylight_time_disabled: u8,
 }
 
